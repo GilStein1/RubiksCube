20e938a8d993653187aed61885745ed72f7181aa
GilStein1 <gils.server@gmail.com>
2025-05-24 16:55:04 +0300

    commented the BitmapUtil class

--- app/src/main/java/com/example/my3dproject/BitmapUtil.java
+++ app/src/main/java/com/example/my3dproject/BitmapUtil.java
@@ -16,11 +16,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Utility class for bitmap processing operations including image transformation,
+ * encoding, and loading from various sources.
+ */
 public class BitmapUtil {
 
+	/**
+	 * Standard width for profile pictures in pixels.
+	 */
 	public static final int PROFILE_PICTURE_WIDTH = 128;
+
+	/**
+	 * Standard height for profile pictures in pixels.
+	 */
 	public static final int PROFILE_PICTURE_HEIGHT = 128;
 
+	/**
+	 * Creates a circular bitmap from the input bitmap by cropping it to a square
+	 * and drawing it within a circular mask.
+	 *
+	 * @param bitmap The source bitmap to be converted to circular shape
+	 * @return A new circular bitmap with dimensions equal to the smaller dimension of the input
+	 */
 	public static Bitmap getCircularBitmap(Bitmap bitmap) {
 		int size = Math.min(bitmap.getWidth(), bitmap.getHeight());
 		Bitmap output = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
@@ -36,6 +54,12 @@ public class BitmapUtil {
 		return output;
 	}
 
+	/**
+	 * Converts a bitmap to a Base64 encoded string representation.
+	 *
+	 * @param bitmap The bitmap to be encoded
+	 * @return Base64 encoded string of the bitmap data
+	 */
 	public static String convertTo64Base(Bitmap bitmap){
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
@@ -43,11 +67,22 @@ public class BitmapUtil {
 		return Base64.encodeToString(data, Base64.DEFAULT);
 	}
 
+	/**
+	 * Loads a bitmap from a content URI and automatically corrects its orientation
+	 * based on EXIF data.
+	 *
+	 * @param imageUri The URI of the image to load
+	 * @param contentResolver ContentResolver to access the image data
+	 * @return A properly oriented bitmap loaded from the URI
+	 * @throws IOException If there's an error reading the image or EXIF data
+	 */
 	public static Bitmap getBitmapOutOfImageUri(Uri imageUri, android.content.ContentResolver contentResolver) throws IOException {
+		//gets a bitmap from the uri
 		InputStream inputStream = contentResolver.openInputStream(imageUri);
 		Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
 		inputStream.close();
 
+		//gets the orientation of the image
 		InputStream exifInputStream = contentResolver.openInputStream(imageUri);
 		ExifInterface exif = new ExifInterface(exifInputStream);
 		int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
@@ -60,8 +95,8 @@ public class BitmapUtil {
 			case ExifInterface.ORIENTATION_ROTATE_270: rotationDegrees = 270; break;
 		}
 
-		if (rotationDegrees != 0) {
-			Matrix matrix = new Matrix();
+		if (rotationDegrees != 0) { // if the orientation is not 0 degrees, rotate the image
+			Matrix matrix = new Matrix(); // rotation matrix for the image
 			matrix.postRotate(rotationDegrees);
 			bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 		}
